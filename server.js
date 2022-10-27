8babeb61d3b4e45e75723c9eace8fee307d0b20a
rachael-pluta <rachael.pluta.work@gmail.com>
2022-10-27 21:33:49 +1100

    created server.js file, including requiring dependcies and server routes

--- server.js
+++ server.js
@@ -3,3 +3,28 @@
     // 2. Create routes folder with apiRoutes.js and htmlRoutes.js files
     // 3. npm install express
 
+// Import dependencies
+const express = require('express');
+
+// Import route files
+const apiRoutes = require('./Develop/routes/apiRoutes');
+const htmlRoutes = require('./Develop/routes/htmlRoutes');
+
+// Import express server
+const app = express();
+
+// Set the PORT
+const PORT = process.env.port || 3001;
+
+// Middleware to parse JSON and url encoded form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use(express.static('public'));
+app.use('./api', apiRoutes);
+app.use('./api', htmlRoutes);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
