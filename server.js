974671c4c8abd806d4eb97c32575df9cb748a050
rachael-pluta <rachael.pluta.work@gmail.com>
2022-10-28 17:08:07 +1100

    set up html and api routes

--- server.js
+++ server.js
@@ -5,10 +5,9 @@
 
 // Import dependencies
 const express = require('express');
-
-// Import route files
-const apiRoutes = require('./Develop/routes/apiRoutes');
-const htmlRoutes = require('./Develop/routes/htmlRoutes');
+const fs = require('fs');
+const path = require('path');
+const noteEntries = require('./Develop/db/db.json');
 
 // Import express server
 const app = express();
@@ -19,12 +18,43 @@ const PORT = process.env.port || 3001;
 // Middleware to parse JSON and url encoded form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
 app.use(express.static('public'));
-app.use('./api', apiRoutes);
-app.use('./api', htmlRoutes);
 
-app.listen(PORT, () =>
+// HTML routes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// Sets the function to create a new note
+function newNoteEntry (body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+
+// Post route for submitting notes
+app.post('/api/notes', (req, res) => {
+    const newNote = newNoteEntry(req.body, noteEntries);
+    res.json(newNote);
+});
+
+app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+});
